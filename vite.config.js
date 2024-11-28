9af7adce67079874c32861ba98909566bcf4c237
MuskuTiwari <166945181+MuskuTiwari@users.noreply.github.com>
2024-11-28 22:28:38 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      "/api": "https://notes4you-server.onrender.com",
+      "/api": "https://notes4you-server.vercel.app",
     },
   },
 });
